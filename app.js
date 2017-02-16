b4489a1f2d24f8d2b8f6f9f69d888948f28dc13d
antyneskul <antyneskul@gmail.com>
2017-02-16 14:40:33 +0100

    added helmet module and refactored to ES6 syntax

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require('express'),
-    methodOverride = require('method-override'),
-    restful = require('node-restful'),
-    bodyParser = require('body-parser'),
-    logger = require('morgan'),
-    mongoose = restful.mongoose,
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    models = require('./schemas');
-var app = express();
+const express = require('express');
+const methodOverride = require('method-override');
+const restful = require('node-restful');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = restful.mongoose;
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const models = require('./schemas');
+const helmet = require('./');
+const app = express();
+
 
 //setup routes
-var index = require('./routes/index');
-var users = require('./routes/users');
-var setuppage = require('./routes/setup');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const setuppage = require('./routes/setup');
 
 
 //setup views engine
@@ -20,10 +22,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('env', 'development');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(cookieParser());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,23 +36,27 @@ app.use('/users', users);
 app.use('/setup', setuppage);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 mongoose.connect('mongodb://localhost/m');
 
-var Resource = app.resource = restful.model('Project', mongoose.Schema({name: String, index: String})).methods(['get', 'post', 'put', 'delete']);
+const Resource = app.resource = restful.model('Project', mongoose.Schema({
+    name: String,
+    index: String
+})).methods(['get', 'post', 'put', 'delete']);
+
 Resource.register(app, '/re');
 
 module.exports = app;
--- test/testReportLoader.js
+++ test/testReportLoader.js
@@ -1,57 +1,56 @@
-var schema = require('./../schemas');
-var dropAllReports = false;
+const schema = require('./../schemas');
+const dropAllReports = false;
 
-function getRandomProject(project) {
-    return new Promise(function (resolve, reject) {
-        project.count().exec(function (err, count) {
-            var rand = Math.floor(Math.random() * count);
-            project.findOne().skip(rand).exec(function (err, data) {
-                if (err) reject(err);
-                else resolve(data);
-            });
+const getRandomProject = (project) => new Promise((resolve, reject) => {
+    project.count().exec((err, count) => {
+        const rand = Math.floor(Math.random() * count);
+        project.findOne().skip(rand).exec((err, data) => {
+            if (err) reject(err);
+            else resolve(data);
         });
     });
-}
+});
 
-function getRandomMetricGroupAndMetrics(metricGroup, metric) {
-    return new Promise(function (resolve, reject) {
-        let mGroup, metrics;
-        metricGroup.count().exec(function (err, count) {
-            let rand = Math.floor(Math.random() * count);
-            metricGroup.findOne().skip(rand).exec(function (err, data) {
-                if (err) reject(err);
-                else {
-                    mGroup = data;
-                    metric.find({metricGroup: data._id}).exec(function (err, data) {
-                        if (err) reject(err);
-                        else {
-                            metrics = data;
-                            resolve({metricGroup: mGroup, metrics: metrics});
-                        }
-                    });
-                }
-            });
+
+const getRandomMetricGroupAndMetrics = (metricGroup, metric) => new Promise((resolve, reject) => {
+    let mGroup, metrics;
+    metricGroup.count().exec((err, count) => {
+        let rand = Math.floor(Math.random() * count);
+        metricGroup.findOne().skip(rand).exec((err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                mGroup = data;
+                metric.find({metricGroup: data._id}).exec((err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        metrics = data;
+                        resolve({metricGroup: mGroup, metrics: metrics});
+                    }
+                });
+            }
         });
     });
-}
+});
 
 
-module.exports = function (store, numberOfRecords) {
+module.exports = (store, numberOfRecords) => {
     const project = store.model('Project', schema.projectSchema);
     const metricGroup = store.model('MetricGroup', schema.metricGroupSchema);
     const metric = store.model('Metric', schema.metricSchema);
-    const metricReport = store.model('MetricReport', schema.metricReportSchema);  
-    
+    const metricReport = store.model('MetricReport', schema.metricReportSchema);
+
     //This also async - I don't really know why did you add it, so can't really refactor it much
-    if (dropAllReports) metricReport.collection.drop(); 
-    
+    if (dropAllReports) metricReport.collection.drop();
+
     Promise
         .all([getRandomProject(project), getRandomMetricGroupAndMetrics(metricGroup, metric)])
         .then(([projectData, metricData]) => {
-          //Here you can do you loop 
-          //You have projectData available here, metricData available as well
+            //Here you can do you loop
+            //You have projectData available here, metricData available as well
         })
-        .catch(e => 
+        .catch(e => {
             throw new Error(e);
-        })
-}
+        });
+};
